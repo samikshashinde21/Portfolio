10c60b89a7a92542f58e343a91dcc532cb22f19c
samikshashinde21 <ssshinde.omk@gmail.com>
2024-02-24 20:27:10 +0530

    Fixed npm dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
+//import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import Education from "./components/education/Education";
 import Projects from "./components/projects/Projects";
